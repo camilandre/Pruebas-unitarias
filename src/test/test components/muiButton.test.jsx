0c14f7056bb05eb544b918306f7dc60f900d407e
Camila Sanchez <camilaandrea.sanchez.pineda.tech@bbva.com>
2024-06-19 16:58:17 +0200

    Separacion de test en carpetas

---
+++ src/test/test components/muiButton.test.jsx
@@ -0,0 +1,11 @@
+
+import { describe, expect, test, vi } from "vitest";
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { Button } from '@mui/material';
+
+describe('Test mui button', () => {
+    test('render muibutton with correct text', () => {
+
+    });
+});
